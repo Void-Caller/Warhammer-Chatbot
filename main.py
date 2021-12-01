a840c6b957965093a81675469fca79a6cb5126cb
Void-Caller <65402334+Void-Caller@users.noreply.github.com>
2021-12-01 18:21:52 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import rollers
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     for i in range(20):
-        rollers.skill_roll(55)
+        rollers.attack_roll(55)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- rollers.py
+++ rollers.py
@@ -6,7 +6,39 @@ def skill_roll(chance):
     if roll <=chance:
         degree = (chance - roll)//10
         print("Roll: %d Success with %d degrees" % (roll,degree))
+        return 1, roll, degree
     else:
         degree = (roll - chance)//10
         print("Roll: %d Failure with %d degrees" % (roll,degree))
+        0, roll, degree
+
+def attack_roll(chance):
+    die = random.randint(0,9)
+    die10 = random.randint(0, 9)
+    roll = (die10*10+die)
+    loc = (die*10+die10)
+    location = ""
+    if loc and loc <16:
+        location = "Head"
+    elif loc <36:
+        location = "Right Arm"
+    elif loc <56:
+        location = "Left Arm"
+    elif loc < 81:
+        location = "Body"
+    elif loc < 91:
+        location = "Right Leg"
+    else:
+        location = "Left Leg"
+
+    if roll and roll <=chance:
+        degree = (chance - roll)//10
+        print("Roll: %d Success with %d degrees." % (roll,degree), end=" ")
+        print("You hit the: "+location)
+        return 1, roll, degree, location
+    else:
+        degree = (roll - chance)//10
+        print("Roll: %d Failure with %d degrees" % (roll,degree))
+        return 0, roll, degree, location
+
 
