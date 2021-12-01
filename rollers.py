406e15b583d2e6f77350f8305bbb4191cd3b4ad9
Void-Caller <65402334+Void-Caller@users.noreply.github.com>
2021-12-01 19:40:25 +0100

    Add files via upload

--- rollers.py
+++ rollers.py
@@ -41,4 +41,26 @@ def attack_roll(chance):
         print("Roll: %d Failure with %d degrees" % (roll,degree))
         return 0, roll, degree, location
 
+def magic_roll(power=1, casting_number = 10, bonus=0, winds=0):
+    dice = []
+    roll = bonus
+    dubs = 0
+    trips = 0
+    quads = 0
+    for i in range(power):
+        die = random.randint(1,10)
+        dice.append(die)
+        roll += winds
+        roll += die
+
+    if roll <= casting_number:
+        print("Roll: %d Success" % (roll))
+        return 1, roll
+    else:
+        print("Roll: %d Failure" % (roll))
+        return 0, roll
+
+
+
+
 
