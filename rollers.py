a20aa2e6c42c075bb398b01bd7fc023de5fc91c1
Void-Caller <65402334+Void-Caller@users.noreply.github.com>
2021-12-15 00:59:33 +0100

    Add files via upload

--- rollers.py
+++ rollers.py
@@ -44,21 +44,44 @@ def attack_roll(chance):
 def magic_roll(power=1, casting_number = 10, bonus=0, winds=0):
     dice = []
     roll = bonus
+
+    #tablica powtórzeń danej liczby
+    numbers = [0 for x in range(10)]
+
     dubs = 0
     trips = 0
     quads = 0
     for i in range(power):
         die = random.randint(1,10)
         dice.append(die)
+        numbers[die-1] += 1
         roll += winds
         roll += die
 
-    if roll <= casting_number:
-        print("Roll: %d Success" % (roll))
-        return 1, roll
+    if numbers[0] == power:
+        print("Automatic Failure!")
+        return 1, roll, dubs, trips, quads
+
+    for number in numbers:
+        if number == 2:
+            dubs += 1
+        elif number == 3:
+            trips += 1
+        elif number >= 4:
+            quads += 1
+
+    if numbers[0] == power:
+        print("Automatic Failure! Dubs: %d, Trips: %d, Quads: %d" % (dubs, trips, quads))
+        return 0, roll, dubs, trips, quads
+
+    if roll >= casting_number:
+        print("Roll: %d, Success! Dubs: %d, Trips: %d, Quads: %d" % (roll, dubs, trips, quads))
+        return 1, roll, dubs, trips, quads
     else:
-        print("Roll: %d Failure" % (roll))
-        return 0, roll
+        print("Roll: %d, Failure! Dubs: %d, Trips: %d, Quads: %d" % (roll, dubs, trips, quads))
+        return 0, roll, dubs, trips, quads
+
+
 
 
 
